a7c094f15b0bf3aa1c8f2badf70e2f086262adce
WilliamFoltz <60475228+WilliamFoltz@users.noreply.github.com>
2020-02-07 23:00:29 -0500

    Home Changes
    Changes made to the "Home" component.
    
    Added a header and footer
    
    Added functional buttons to swap between pages
    
    Added backgorund image
    
    Started to create drop down menu to replace buttons

--- angularsliceproject2/src/app/home/home.component.ts
+++ angularsliceproject2/src/app/home/home.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+  
+    homeBool:boolean = false;
+    menuBool:boolean = false;
+    checkBool:boolean = false;
+    contactBool:boolean = false;
+    picBool:boolean = true;
+
 
   ngOnInit() {
   }
 
+  showHome(){
+    this.picBool = true;
+    this.menuBool = false;
+    this.checkBool = false;
+    this.contactBool = false;
+  }
+
+  showMenu(){
+    this.homeBool = false;
+    this.menuBool = true;
+    this.checkBool = false;
+    this.contactBool = false;
+    this.picBool = false;
+  }
+  showCheckout(){
+    this.homeBool = false;
+    this.menuBool = false;
+    this.checkBool = true;
+    this.contactBool = false;
+    this.picBool = false;
+  }
+  showContact(){
+    this.homeBool = false;
+    this.menuBool = false;
+    this.checkBool = false;
+    this.contactBool = true;
+    this.picBool = false;
+  }
 }
