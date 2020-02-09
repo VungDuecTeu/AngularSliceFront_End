8a0630a5ae6445c6f2e8a5b58f92a98a2e82b4de
Bush Lam <bushlam28@gmail.com>
2020-02-09 16:35:21 -0500

    mat

--- angularsliceproject2/src/app/contact/contact.component.ts
+++ angularsliceproject2/src/app/contact/contact.component.ts
@@ -1,17 +1,47 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {} from 'googlemaps';
+import { ThemePalette } from '@angular/material';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
 export class ContactComponent implements OnInit {
 
   constructor() { }
 
   @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
   map: google.maps.Map;
+
+
+  color: ThemePalette = 'primary';
+  value = 50;
+  bufferValue = 75;
+  
+  fooditemscollapse = [false,false,false,false,false,false];
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+  fooditemCollapse(item:number){
+    for (let i:number = 0; i < this.fooditemscollapse.length; i++){
+      this.fooditemscollapse[i] = false;
+    }
+
+      this.fooditemscollapse[item] = !this.fooditemscollapse[item];
+    }
 
   ngOnInit() {
     setTimeout(()=> {
