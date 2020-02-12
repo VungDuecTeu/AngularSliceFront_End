0c3bb6f1a13f725114a95dc738dca891c3fc4401
inamjr <ikeogu56@gmail.com>
2020-02-11 21:37:07 -0600

    fixed table
    new table

--- angularsliceproject2/src/app/checkout/checkout.component.ts
+++ angularsliceproject2/src/app/checkout/checkout.component.ts
@@ -1,18 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
-export interface PeriodicElement {
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+
+// }
+interface Food {
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  image: string;
+  callories: number;
+  price: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Peperoni pizza', weight: 500, symbol: '$20.50'},
-  {position: 2, name: 'Cheesesticks', weight: 300, symbol: '$10.15'},
-  {position: 3, name: 'Soda', weight: 60, symbol: '$2.00'},
-  {position: 4, name: 'Brownie', weight: 500, symbol: '$5.00'},
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Peperoni pizza', weight: 500, symbol: '$20.50'},
+//   {position: 2, name: 'Cheesesticks', weight: 300, symbol: '$10.15'},
+//   {position: 3, name: 'Soda', weight: 60, symbol: '$2.00'},
+//   {position: 4, name: 'Brownie', weight: 500, symbol: '$5.00'},
  
+// ];
+
+const FOOD: Food[] = [
+  {
+    name: 'Peperoni PizzaS',
+    image: '/menu_item_280x175/public/images/menu-items/thumbnails/build-you-own_0.png?itok=kqLw4gRp',
+    callories: 280,
+    price: '$12.50'
+  },
+  {
+    name: 'Sausage & Peperoni Pizza',
+    image: '/menu_item_280x175/public/images/menu-items/thumbnails/meat-eaters.png?itok=X5GvwbNj',
+    callories: 300,
+    price: '$15.25'
+  },
+  {
+    name: 'Soda',
+    image: '/menu_item_280x175/public/images/menu-items/thumbnails/46._2_liter_beverage.png?itok=k_Oh2-Tc',
+    callories: 50,
+    price: '$5.00'
+  },
+  {
+    name: 'Wings',
+    image: '/menu_item_280x175/public/images/menu-items/thumbnails/34._bbq_howie_wings.png?itok=zj3a0XQI',
+    callories: 100,
+    price: '$20.25'
+  }
 ];
 
 @Component({
@@ -25,8 +59,9 @@ const ELEMENT_DATA: PeriodicElement[] = [
 export class CheckoutComponent implements OnInit {
 
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  foods = FOOD;
+  // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  // dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit() {
