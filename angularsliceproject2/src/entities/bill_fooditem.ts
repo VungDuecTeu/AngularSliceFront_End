aec6439f31d6ae4c5bdc9c51e8cca5600d3f983e
WilliamFoltz <60475228+WilliamFoltz@users.noreply.github.com>
2020-02-14 14:10:06 -0500

    added entities

---
+++ angularsliceproject2/src/entities/bill_fooditem.ts
@@ -0,0 +1,6 @@
+export interface bill_fooditem{
+    id: number;
+    b_id: number;
+    amount: number;
+    f_id: number;
+}
---
+++ angularsliceproject2/src/entities/fooditem.ts
@@ -0,0 +1,10 @@
+export interface fooditem{
+    id: number;
+    foodType: string;
+    name: string;
+    description: string;
+    price: number;
+    calories: number;
+    url: string;
+
+}
