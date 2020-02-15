6adff655c8d41b5666e02289dd161d613d7c7abf
WilliamFoltz <60475228+WilliamFoltz@users.noreply.github.com>
2020-02-15 17:47:54 -0500

    changed header

--- angularsliceproject2/src/app/header/header.component.ts
+++ angularsliceproject2/src/app/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatMenuTrigger} from '@angular/material';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
+  //@ViewChild(MatMenuTrigger) matMenuTrigger: MatMenuTrigger;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // openMyMenu() {
+  //   this.matMenuTrigger.openMenu();
+
+  // } 
+  // closeMyMenu() {
+  //   this.matMenuTrigger.closeMenu();
+  // }
 }
