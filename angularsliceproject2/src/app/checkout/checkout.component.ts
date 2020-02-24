8f00079e14ac2238a524b608e8980d47bd440f9b
Bush Lam <bushlam28@gmail.com>
2020-02-23 20:19:29 -0500

    checkout

--- angularsliceproject2/src/app/checkout/checkout.component.ts
+++ angularsliceproject2/src/app/checkout/checkout.component.ts
@@ -9,6 +9,8 @@ import { DataService } from '../services/data.service';
 import { AccountService } from '../services/accountservice/account.service';
 import { getLocaleDateTimeFormat, DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
+import { BillFooditemService } from '../services/billfooditemservice/Bill_Fooditem.service';
+import { Bill_Fooditem } from '../entities/Bill_Fooditem';
 
 @Component({
   selector: 'app-checkout',
@@ -27,7 +29,8 @@ export class CheckoutComponent implements OnInit {
     private data: DataService, 
     private accountservice: AccountService,
     private bill:BillService,
-    private router:Router
+    private router:Router,
+    private billfoodservice:BillFooditemService
     ) { }
 
   order:Array<Fooditem> = [];
@@ -62,11 +65,14 @@ export class CheckoutComponent implements OnInit {
     console.log(this.total);
   }
 
-  changeTotal(){
-    if (this.isdeliver == 1)
-    this.total = this.fs.total * 1.06 + this.deliveryfee;
+  changeTotal(i:number){
+    if (i == 2)
+      this.deliveryfee = 3;
     else
-    this.total = this.fs.total * 1.06;
+      this.deliveryfee = 0;
+
+    this.total = this.fs.total * 1.06 + this.deliveryfee;
+
   }
 
   setDateTime(dateTime) {
@@ -79,6 +85,15 @@ export class CheckoutComponent implements OnInit {
     return date + ' ' + time;
   }
 
+  async newBillFood(bill:Bill, quantity:any, food:any){
+    let billfood = new Bill_Fooditem(0, bill, quantity, food);  
+    console.log(food);
+
+    await this.billfoodservice.createBillFooditem(billfood).then((onfulfilled)=>{
+      // console.log(onfulfilled);
+    });
+  }
+
   async newbill(){
     if (this.total!=0.00 && this.accountId != 0 ) {
 
@@ -89,12 +104,18 @@ export class CheckoutComponent implements OnInit {
       console.log(Newbill);
       await this.bill.creatBill(Newbill).then((onfulfilled)=>{
         if(onfulfilled != null){
-          alert(" Thanks for Your Buissness Enjoy!!!");
+
+          for (let i:number = 0; i < this.order.length; i++){
+            // console.log(this.orderAmounts[i]);
+            this.newBillFood(onfulfilled, this.orderAmounts[i], this.order[i]);
+          }
+
+
          // this.dataserv.changeAccount(onfulfilled);
             
-            window.location.reload();
+            // window.location.reload();
           //console.log(onfulfilled);
-
+          alert(" Thanks for Your Business Enjoy!!!");
           return onfulfilled;
         }
       }) 
